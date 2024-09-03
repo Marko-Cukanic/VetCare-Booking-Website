92c330d0c83e8c1ae98629f23da3052a34838060
Marko-Cukanic <s4008040@student.rmit.edu.au>
2024-09-03 13:26:13 +1000

    Controller changes

--- src/main/java/au/edu/rmit/sept/webapp/Controller/BookingController.java
+++ src/main/java/au/edu/rmit/sept/webapp/Controller/BookingController.java
@@ -1,4 +1,4 @@
-package au.edu.rmit.sept.webapp.Controller;
+package au.edu.rmit.sept.webapp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
